08c66f4db9c5cfe750218713f64f72e140824ca9
Ange <ange.ingabire07@gmail.com>
2020-08-10 11:51:56 +0200

    making the signup and signin connect to firebase

---
+++ scripts/admin.js
@@ -0,0 +1,8 @@
+var db = firebase.firestore();
+function upload() {
+    //get your image
+    var image = document.getElementById('img').files[0];
+    //get your blog text
+    var post = document.getElementById('postform').value;
+    var title = document.getElementById('title').value
+}
